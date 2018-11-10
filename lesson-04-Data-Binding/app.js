d44fc6d3f635fe8a21ae17f02ad9f48a5f5c65d7
Viking Den <vikingden7@gmail.com>
2018-11-10 10:14:17 +0800

    add Data Binding

---
+++ lesson-04-Data-Binding/app.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#vue-app',
+    data: {
+        website: 'http://vikingden.cn',
+        websiteTag: '<a href="http://vikingden.cn">Viking Den Website</a>',
+        name: 'Viking Den',
+    }
+})
