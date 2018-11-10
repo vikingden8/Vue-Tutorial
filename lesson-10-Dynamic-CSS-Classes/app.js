6faded57f884bc02d1922fa299ae5d8ae0dc0284
Viking Den <vikingden7@gmail.com>
2018-11-10 10:53:07 +0800

    add Dynamic CSS Classes

---
+++ lesson-10-Dynamic-CSS-Classes/app.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#vue-app',
+    data: {
+        available: false,
+        nearby: false
+    },
+    methods: {
+
+    },
+    computed: {
+        compClasses: function(){
+            return{
+                available: this.available,
+                nearby: this.nearby,
+            }
+        }
+    }
+})
